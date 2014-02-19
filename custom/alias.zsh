f2799f2eb8bd66fdc2fa1aa0bdf0a1b66b907cb6
Zach Wolfe <wolfe.zach@gmail.com>
2014-02-19 10:10:56 -0500

    Moved aliases from zshrc to alias.zsh and added new git aliases to git.zsh

---
+++ custom/alias.zsh
@@ -0,0 +1,3 @@
+alias l="ls -1"
+alias ack="ack-grep --ignore-dir=node_modules --ignore-dir=vendor --ignore-dir=bower_components"
+alias c.="cd .."
---
+++ custom/git.zsh
@@ -0,0 +1,4 @@
+alias gs='git status'
+alias gc='git commit'
+alias gpl='git pull origin master'
+alias gps='git push origin master'

e3ddae82f0a716c336e16aaae33d3a86e227ee46
Zach Wolfe <thewolf@amazon.com>
2014-04-16 15:39:59 +0000

    add custom aliases

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,3 +1,5 @@
 alias l="ls -1"
-alias ack="ack-grep --ignore-dir=node_modules --ignore-dir=vendor --ignore-dir=bower_components"
+#alias ack="ack-grep --ignore-dir=node_modules --ignore-dir=vendor --ignore-dir=bower_components"
 alias c.="cd .."
+alias c.l="cd .. ; ls -1"
+alias tmux="TERM=screen-256color-bce tmux"
